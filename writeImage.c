6d7f166e4e23ff53ff5656e839f96d2e3afb5d87
Sujoy Kumar Das <sujoy@archlinux.us>
2012-04-09 11:21:12 +0530

    filename refactoring

--- writeImage.c
+++ writeImage.c
@@ -16,53 +16,37 @@
 #include "writeImage.h"
 
 void writeImage(int ***imageMatrix,int y,int x, int itr) {
-    char filename[50]="";
     char folder[20]="segmented_source";
-
+    char filenames[4][20]={"seg_bck.pgm","seg_csf.pgm","seg_gm.pgm","seg_wm.pgm"};
     int i=0,j=0,k=0;
     FILE *img;
 
+
+
+    if(chdir(folder)!=0) {
+        if (mkdir(folder,(S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH))!=0 && errno!=EEXIST) {
+            printf("cannot create directory: %s -- %s\n",folder,strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        if(chdir(folder)!=0) {
+            printf("cannot enter directory: %s -- %s\n",folder,strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+    }
+
     for (k=0;k<itr;k++) {
 
-        if(chdir(folder)!=0) {
-            /* printf("cannot create directory: %s -- %s\n",folder,strerror(errno));
-               exit(EXIT_FAILURE);*/
-            if (mkdir(folder,(S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH))!=0 && errno!=EEXIST) {
-                printf("cannot create directory: %s -- %s\n",folder,strerror(errno));
-                exit(EXIT_FAILURE);
-            }
-            if(chdir(folder)!=0) {
-                printf("cannot create directory: %s -- %s\n",folder,strerror(errno));
-                exit(EXIT_FAILURE);
-            }
-
-        }
-        switch(k) {
-        case 0:
-            strcpy(filename,"seg_bck.pgm");
-            break;
-        case 1:
-            strcpy(filename,"seg_csf.pgm");
-            break;
-        case 2:
-            strcpy(filename,"seg_gm.pgm");
-            break;
-        case 3:
-            strcpy(filename,"seg_wm.pgm");
-        }
-
-        img=fopen(filename,"wb");
+        img=fopen(filenames[k],"wb");
         if (img == NULL) {
-            printf("cannot open file: %s -- %s\n",filename,strerror(errno));
+            printf("cannot open file: %s -- %s\n",filenames[k],strerror(errno));
             exit(EXIT_FAILURE);
         }
 
         fprintf(img,"P2\n#created by sdr\n181 217\n255\n");
 
-        for(i=0;i<y;i++)
-        {
-            for(j=0;j<x;j++)
-            {
+        for(i=0;i<y;i++) {
+            for(j=0;j<x;j++) {
                 fprintf(img,"%d\t",imageMatrix[k][i][j]);
             }
             fprintf(img,"\n");
@@ -72,8 +56,6 @@ void writeImage(int ***imageMatrix,int y,int x, int itr) {
             printf("%s\n",strerror(errno));
             exit(EXIT_FAILURE);
         }
-
-        chdir("..");
     }
 
 }
